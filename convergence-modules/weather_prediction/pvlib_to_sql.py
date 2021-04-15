27b69e3932816f644cd31ce76c2de98c4816e4e8
Maciek <maciekglowacki@icloud.com>
2021-04-15 16:23:42 +0100

    adding sun position

--- convergence-modules/weather_prediction/pvlib_to_sql.py
+++ convergence-modules/weather_prediction/pvlib_to_sql.py
@@ -3,9 +3,20 @@ from sqlalchemy import create_engine
 import pymysql
 import pandas as pd
 import datetime
+import pvlib
 from pvlib.forecast import GFS, NAM, NDFD, HRRR, RAP
 import os
 import yaml
+
+def get_sun_pos(start, latitude, longitude, tz):
+    dfs = []
+    time = []
+    time.append(start)
+    for i in range(1,48):
+        time.append(time[i-1] + pd.Timedelta(minutes=30))
+        dfs.append(pvlib.solarposition.get_solarposition(time[i-1], latitude, longitude, altitude=None, pressure=None, method='nrel_numpy', temperature=12))
+    df = pd.concat(dfs)
+    return (df)
 
 def main(config):
 
@@ -21,6 +32,7 @@ def main(config):
             print(f'KeyError! Must provide: {key}')
             exit()
    
+   
     tableName = params['name']   
     latitude, longitude, tz = params['location'][0],params['location'][1], 'Etc/Greenwich'
     
@@ -30,8 +42,10 @@ def main(config):
         date = params['time']
         start = pd.Timestamp(datetime.datetime(int(date.split("-")[0]),int(date.split("-")[1]),int(date.split("-")[2])), tz=tz)
     end = start + pd.Timedelta(days=params['period'])
-    
+
     print(f"starting:{start},ending:{end}")
+    df_sun_pos = get_sun_pos(start, latitude, longitude, tz).reset_index().rename(columns={'index':'timestamp'})
+   
     model = GFS()
     dataFrame = model.get_processed_data(latitude, longitude, start, end).reset_index().rename(columns={'index':'timestamp'})
     dataFrame.drop(columns=['low_clouds', 'mid_clouds', 'high_clouds'], inplace=True)
@@ -39,13 +53,14 @@ def main(config):
     dataFrame.drop(columns=['timestamp'], inplace=True)
     dataFrame = dataFrame.resample('30T').interpolate(method='linear') #change to 30min timestamp and use linear interpolation 
     dataFrame=dataFrame.reset_index().rename(columns={'index':'timestamp'})
+    df=dataFrame.merge(df_sun_pos, on=['timestamp'])
      
     sqlEngine = create_engine(f"mysql+pymysql://{params['username']}:{params['password']}@localhost/convergence_test",pool_recycle=3600)
 
     dbConnection = sqlEngine.connect()
 
     try:
-        frame = dataFrame.to_sql(tableName, dbConnection, if_exists='replace', index=False);
+        frame = df.to_sql(tableName, dbConnection, if_exists='replace', index=False);
     except ValueError as vx:
         print(vx)
     except Exception as ex:   
@@ -54,6 +69,7 @@ def main(config):
         print("Table %s created successfully."%tableName);   
     finally:
         dbConnection.close()
+   
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
