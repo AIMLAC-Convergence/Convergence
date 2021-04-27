850b5bd0189c8242a27d6f2a19d1ed6879477885
frankmnb <75021303+frankmnb@users.noreply.github.com>
2021-04-27 15:25:22 +0100

    converted error mitigation to semi-hourly working

--- convergence-modules/error_min/error_mitigation.py
+++ convergence-modules/error_min/error_mitigation.py
@@ -1,36 +1,47 @@
 import numpy as np
+import math
 
 def error_mitigation(temp,wind,sunrise,sunset):
     '''
-    temp - array of temperature throughout the day (^o C)
-    wind - array of wind speeds throughout the day (metres/s)
-    sunrise/sunset to nearest hour
+    temp - array of temperature semihourly for 24hrs (Centigrade)
+    wind - array of wind speeds semihourly for 24hrs (metres/sec)
+    sunrise/sunset to nearest half hour
     '''
     
-    panel_efficiency = 0.196
-    pmax = -0.0037
+    panel_efficiency = 0.196 # Output efficiency of panel
+    pmax = -0.0037 # Percentage output lost per degree above nominal operating temp
 
     temp_act = 0
     eff_calc = 0
 
-    for i in range(24):
+    for i in range(48):
 
-        if i >= (sunrise + 1) and i <= (sunset +1):
+        # Calculate the actual temperature of panel
+        if i >= (sunrise*2 + 1) and i <= ((sunset*2) +1):
             temp_act = temp[i] + 20 # accommodate for panel heating up during the day
             eff_calc = panel_efficiency - ((temp_act-25)*pmax)
         else:
             eff_calc = panel_efficiency - (temp[i]*pmax) # cool during the night
 
         if eff_calc > panel_efficiency:
-            eff_calc = panel_efficiency
+            eff_calc = panel_efficiency # Can't be more efficient than rated in negative temp
 
-        if i < 10:
-            print("0{0}:00: Solar panel efficiency = {1}%   ({2}%)".format(i,"%0.2f" %(eff_calc*100),"%0.2f" %((eff_calc/panel_efficiency)*100)))
+        # Calculate panel efficiency throughout the day accounting for temp
+        if i%2 == 0:
+            if i>24:
+                print("{0}:00: Solar panel efficiency = {1}%   ({2}%)".format(math.floor(i/2),"%0.2f" %(eff_calc*100),"%0.2f" %((eff_calc/panel_efficiency)*100)))
+            elif i<19:
+                print("0{0}:00: Solar panel efficiency = {1}%   ({2}%)".format(math.floor(i/2),"%0.2f" %(eff_calc*100),"%0.2f" %((eff_calc/panel_efficiency)*100)))
+            else:
+                print("{0}:00: Solar panel efficiency = {1}%   ({2}%)".format(math.floor(i/2),"%0.2f" %(eff_calc*100),"%0.2f" %((eff_calc/panel_efficiency)*100)))
         else:
-            print("{0}:00: Solar panel efficiency = {1}%   ({2}%)".format(i,"%0.2f" %(eff_calc*100),"%0.2f" %((eff_calc/panel_efficiency)*100)))
-                
+            if i>20:
+                print("{0}:30: Solar panel efficiency = {1}%   ({2}%)".format(math.floor(i/2),"%0.2f" %(eff_calc*100),"%0.2f" %((eff_calc/panel_efficiency)*100)))
+            else:
+                print("0{0}:30: Solar panel efficiency = {1}%   ({2}%)".format(i-math.ceil(i/2),"%0.2f" %(eff_calc*100),"%0.2f" %((eff_calc/panel_efficiency)*100)))
+                   
         # Warnings
-        if i > sunrise and i < sunset:
+        if i > sunrise*2 and i < sunset*2:
             if temp_act > 35 or temp_act < 15:
                 print('`--->  Solar panel output will be affected by temperature.')
 
@@ -44,8 +55,8 @@ def error_mitigation(temp,wind,sunrise,sunset):
         print('**********')
 
 
-temp = np.array([1,0,0,0,0,0,0,1,2,4,6,-7,8,8,19,19,9,8,8,8,6,5,4,3]) # temperature degrees centigrade
-wind = np.array([1,1,1,1,1,1,1,1,10,15,18,1,1,26,1,2,2,2,2,62,10,19,1,1]) # wind speed in m/s
+temp = np.array([7,7,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,5,5,5,5,5,5]) # temperature degrees centigrade
+wind = np.array([2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,4,4,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,5,5,4,4,5,5,4,4,4,4,4,4,4,4]) # wind speed in m/s
 sunrise = 7
 sunset = 20
 
