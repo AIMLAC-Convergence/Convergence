f1fef5b161640bd50a43f40a8c6cf5da3a655926
aber-wgr <wgr@aber.ac.uk>
2021-08-02 09:53:15 +0100

    imports for model class

--- Model/scripts/rnn_model.py
+++ Model/scripts/rnn_model.py
@@ -1,3 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import ast
+import time
+from datetime import date, timedelta
+import requests
+
 class RNN_Model:
     def __init__(self, dataframe, time_lag):
         self._dataframe = dataframe
